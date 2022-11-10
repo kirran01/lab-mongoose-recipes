0ec77be85e01ac15db2e9906d440a58dae383fb8
kirran01 <Kirran-@live.com>
2022-11-10 17:56:15 -0500

    iteration 3

--- index.js
+++ index.js
@@ -13,29 +13,30 @@ mongoose
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    // return Recipe.deleteMany()
+    return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+   return Recipe.create({
+      title: "chicken",
+      level: "Easy Peasy",
+      ingredients: ["chicken"],
+      cuisine: "meat",
+      dishType: "other",
+      image:
+        "https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg",
+      duration: 10,
+      creator: "me",
+    })
+  })
+  .then((savedRecipe) => {
+    console.log("recipe title:", savedRecipe.title);
+    return Recipe.insertMany(data);
+  })
+  .then((newRecipes) => {
+    newRecipes.forEach((recipe) => {
+      console.log(recipe.title);
+    });
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
-
-Recipe.create({
-  title: "chicken",
-  level: "Easy Peasy",
-  ingredients: ["chicken"],
-  cuisine: "meat",
-  dishType: "other",
-  image:
-    "https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg",
-  duration: 10,
-  creator: "me",
-  created: Date.now(),
-})
-  .then((savedRecipe) => {
-    console.log("recipe title:", savedRecipe.title);
-    return Recipe.find();
-  })
-  .catch((err) => console.log(err));
